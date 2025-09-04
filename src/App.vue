eaa9776f2d3b2ffa2aac83dfce429ba11a37fb77
yxie0087 <yxie0087@student.monash.edu>
2025-09-04 21:59:37 +1000

    fix

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import ThemeToggle from './components/ThemeToggle.vue'
 <template>
   <header class="section" style="padding: .75rem 0; border-bottom: 1px solid color-mix(in oklab, var(--fg) 10%, transparent); position: sticky; top: 0; background: var(--bg); z-index: 50;   ">
     <div class="container" style="display:flex; align-items:center; gap:.75rem; margin:0 auto; max-width: 1300px; ">
-      <RouterLink><img src="@/assets/logo.svg" /></RouterLink>
+      <RouterLink to="/"><img src="@/assets/logo.svg" /></RouterLink>
       <RouterLink to="/" style="font-weight: 800;">Plant'X</RouterLink>
       <nav style="margin-left:auto; display:flex; gap:.99rem; font-size: larger;">
         <RouterLink to="/">Home</RouterLink>
